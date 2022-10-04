e325abf10867dd727db498eb9181fd28b9242fa3
PDundh <dundh.parth@gmail.com>
2022-10-04 19:12:16 +0530

    fixed graph generation issues

--- graphGenerate.py
+++ graphGenerate.py
@@ -9,19 +9,31 @@ def generate(nodes, edge_limit, cost_limit):
     line = list(range(0, nodes))
     r.shuffle(line)
 
+    print(line)
+
     # add random list to adjecent matrix
-    for i in range(0, nodes-2):
-        value = round(r.uniform(0,cost_limit))
+    for i in range(0, nodes-1):
+        value = round(r.uniform(1,cost_limit))
+        print(value)
         matrix[line[i]][line[i+1]] = value
         matrix[line[i+1]][line[i]] = value
 
+    # complete the ring
+    matrix[line[0]][line[nodes-1]] = value
+    matrix[line[nodes-1]][line[0]] = value
+
     # each iteration addes atmost 1 connection per node
-    for _ in range(0,edge_limit-2):
+    for _ in range(0,edge_limit-1):
         r.shuffle(line)
         
         if nodes%2!=0 : nodes=nodes-1
         for i in range(0,nodes):
-            value = round(r.uniform(0,cost_limit))
+            
+            # leave already made connections untouched
+            if matrix[line[i]][line[nodes-1-i]] != 0:
+                continue
+
+            value = round(r.uniform(1,cost_limit))
             matrix[line[i]][line[nodes-1-i]] = value
             matrix[line[nodes-1-i]][line[i]] = value
 
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from select import select
 import graphGenerate as grph
 import visual as draw
 import bellman as bell
 import dijkstra as dk
 
-nodes = 24
+nodes = 7
 edge_limit = 3
 cost_limit = 4
 
@@ -18,21 +17,25 @@ def getPath(path_list, src, dest):
     path.reverse()
     return path
 
-matrix = grph.generate(nodes,edge_limit, cost_limit)
-graph = draw.showGraph(matrix)
+def run():
+
+    matrix = grph.generate(nodes,edge_limit, cost_limit)
+    graph = draw.showGraph(matrix)
+
+    return
+
+    src = int(input("enter source node => "))
+    dest = int(input("enter destination node => "))
+    algo = input("enter choice: 1.Bellman-Ford 2.Dijkstra => ")
+
+    if algo == '1': shortest = bell.bellman(matrix, src)
+    else          : shortest = dk.dijkstra(matrix,src)
+
+    path = getPath(shortest, src, dest)
+
+    print("cost =>",str(shortest[dest][1]))
+    draw.highlight(graph,path)
 
 
-src = int(input("enter source node => "))
-dest = int(input("enter destination node => "))
-algo = input("enter choice: 1.Bellman-Ford 2.Dijkstra => ")
-
-if algo == '1': shortest = bell.bellman(matrix, src)
-else          : shortest = dk.dijkstra(matrix,src)
-
-path = getPath(shortest, src, dest)
-
-print("cost =>",str(shortest[dest][1]))
-draw.highlight(graph,path)
-
-# TODO
-# graph generation is giving disconnected graphs
+for _ in range(5):
+    run()
